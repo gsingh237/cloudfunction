477284bf936618a555f759884ae57ff5895e8835
gsingh237 <gurpartap.singh@coforge.com>
2023-12-06 16:52:19 +0530

    updated Readme.md

---
+++ README.md
@@ -0,0 +1,1 @@
+Replace placeholders like project_id, bucket_name, service_account etc with your account values.
--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "google" {
     credentials = file("credentials/service-account-key.json")
-    project     = "your-project-id"
-    region      = "us-central1" # Default region
+    project     = "project_id"
+    region      = "us-central1" # This will be the default region
 }
 
 resource    "google_cloudfunctions_function" "example" {
     name        = "example-function"
     runtime     = "nodejs14"
-    source_archive_bucket   = "bucket-name"
+    source_archive_bucket   = "bucket_name"
     source_archive_object   = "function_code.zip"
     entry_point             = "handler"
     event_trigger   {
